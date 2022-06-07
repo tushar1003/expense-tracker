88884b2fe00470fcb8a3439ca168215500c76dc3
tushar1003 <91830386+tushar1003@users.noreply.github.com>
2022-06-07 17:47:07 +0530

    expense-tracker

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+function ExpenseItem(props){
+    // const [title,setTitle]=useState(props.title);
+    //     const clickHandler=()=>{
+    //         setTitle('Clicked!!')
+    //         console.log(title);
+    //     };
+
+       
+    return ( 
+    <Card className="expense-item">
+        <ExpenseDate date={props.date}/>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">&#8377;{props.amount }</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
+    </Card>
+    );
+    }
+export default ExpenseItem;
